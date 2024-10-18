5efb3d910c9e9dfe27610b4cdb5a92ad0dd65efe
Christiyan Yankov <krisyankov@gmail.com>
2024-10-18 12:31:03 +0300

    Added the basic order class.

---
+++ WebApp/Order.py
@@ -0,0 +1,6 @@
+class Order:
+    def __init__(self, table, datetime, products, totalPrice):
+        self.table = table
+        self.datetime = datetime
+        self.products = products
+        self.totalPrice = totalPrice
