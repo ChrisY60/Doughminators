803a5508ca3dfe5950f181e8d88a585b0eb7da77
Кристиян Янков <39673325+ChrisY60@users.noreply.github.com>
2024-10-30 11:49:51 +0200

    Fixed the app.py

--- WebApp/app.py
+++ WebApp/app.py
@@ -2,112 +2,10 @@ from flask import Flask, render_template, jsonify
 from datetime import datetime
 
 from Classes.Topping import *
-
-
-
-
-
-# class Topping:
-#     def __init__(self, name, price, description=""):
-#         self.name = name
-#         self.price = price
-#         self.description = description
-
-#     def to_dict(self):
-#         return {
-#             "name": self.name,
-#             "price": self.price,
-#             "description": self.description
-#         }
-
-class Product:
-    def __init__(self, name, price, description, imageURL):
-        self.name = name
-        self.price = price
-        self.description = description
-        self.imageURL = imageURL
-
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "price": self.price,
-            "description": self.description,
-            "imageURL": self.imageURL
-        }
-
-
-class Pizza(Product):
-    def __init__(self, name, price, description, imageURL, crust, base, size, toppings):
-        super().__init__(name, price, description, imageURL)
-        self.crust = crust
-        self.base = base
-        self.size = size
-        self.toppings = toppings
-
-    def add_topping(self, topping):
-        self.toppings.append(topping)
-        self.price += topping.price
-
-    def remove_topping(self, topping):
-        if topping in self.toppings:
-            self.toppings.remove(topping)
-            self.price -= topping.price
-
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "price": self.price,
-            "description": self.description,
-            "imageURL": self.imageURL,
-            "crust": self.crust,
-            "base": self.base,
-            "size": self.size,
-            "toppings": [topping.to_dict() for topping in self.toppings]
-        }
-
-
-
-class Beverage(Product):
-    def __init__(self, name, price, description, imageURL, milliliters):
-        super().__init__(name, price, description, imageURL)
-        self.milliliters = milliliters
-
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "price": self.price,
-            "description": self.description,
-            "imageURL": self.imageURL,
-            "milliliters": self.milliliters
-        }
-
-
-class Order:
-    STATUS_OPTIONS = ["TO DO", "COOKING", "READY FOR SERVING", "SERVED"]
-    def __init__(self, id, table, datetime, products, totalPrice, status):
-        self.id = id
-        self.table = table
-        self.datetime = datetime.now()
-        self.products = products
-        self.totalPrice = totalPrice
-        self.status = "TO DO"
-
-    def update_totalPrice(self):
-        self.totalPrice = sum(item.price for item in self.products)
-
-    def add_product(self, product):
-        self.products.append(product)
-        self.totalPrice += product.price
-
-    def remove_product(self, product):
-        self.products.remove(product)
-        self.totalPrice -= product.price
-
-    def set_status(self, status):
-        if status in self.STATUS_OPTIONS:
-            self.status = status
-        else:
-            raise ValueError("Status must be one of: " + ", ".join(self.STATUS_OPTIONS))
+from Classes.Product import *
+from Classes.Pizza import *
+from Classes.Beverage import *
+from Classes.Order import *
 
 app = Flask(__name__)
 app.secret_key = 'DoughminatorsKey'
@@ -146,13 +44,13 @@ def beverages_page():
 
 @app.route("/makeOrder")
 def makeOrder():
-    items = [pizzas[0], beverages[0]]  # Add one pizza and one beverage to the order
+    items = [pizzas[0], beverages[0]]
     current_time = datetime.now().strftime("%H:%M:%S")
 
     # Create an order instance
     order = Order(1, 4, current_time, items, sum(item.price for item in items), "TO DO")
 
-    return jsonify(order.__dict__)  # Convert the order object to JSON
+    return jsonify(order.__dict__)
 
 if __name__ == '__main__':
     app.run(port=8080)
